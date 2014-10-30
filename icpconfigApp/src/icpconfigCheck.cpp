50ada7aa3b077d5a6d2959e2950f282e778f61d3
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2014-10-30 11:26:51 +0000

    Fist go at a configuration checker
    Former-commit-id: 94c17098bbdab52ec0cc0c3993e3a402534df236

---
+++ icpconfigApp/src/icpconfigCheck.cpp
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <math.h>
+#include <exception>
+#include <algorithm>
+#include <stdexcept>
+#include <iostream>
+#include <map>
+#include <list>
+#include <string>
+#include <time.h>
+#include <sstream>
+#include <fstream>
+
+#include "epicsStdlib.h"
+#include "epicsString.h"
+#include "dbDefs.h"
+#include "epicsMutex.h"
+#include "dbBase.h"
+#include "dbStaticLib.h"
+#include "dbFldTypes.h"
+#include "dbCommon.h"
+#include "dbAccessDefs.h"
+#include <epicsTypes.h>
+#include <epicsTime.h>
+#include <epicsThread.h>
+#include <epicsString.h>
+#include <epicsTimer.h>
+#include <epicsMutex.h>
+#include <iocsh.h>
+#include <initHooks.h>
+#include "envDefs.h"
+#include "macLib.h"
+#include "errlog.h"
+#include "dbAccess.h"
+#include "dbTest.h"
+#include "dbStaticLib.h"
+#include "errlog.h"
+
+#ifdef _WIN32
+#include <io.h>
+#else
+#include <unistd.h>
+#endif
+
+#include "pugixml.hpp"
+
+#include "utilities.h"
+
+#include "icpconfig.h"
+
+#include <epicsExport.h>
+
+int main(int argc, char* argv[])
+{
+    std::string configName, ioc_name, ioc_group, configHost, configBase;
+//	configHost = getenv("COMPUTERNAME");
+//	configBase = "c:/instrument/settings/config";
+	int options = VerboseOutput;
+	if (argc > 1)
+	{
+	    configName = argv[1];
+	}
+	if (argc > 2)
+	{
+	    ioc_name = argv[2];
+	}
+	if (argc > 3)
+	{
+	    configHost = argv[3];
+	}
+    icpconfigCheck(configName, ioc_name, ioc_group, configHost, configBase, options);
+    return 0;
+}
