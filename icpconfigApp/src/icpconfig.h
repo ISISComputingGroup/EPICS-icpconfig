0402426925b0311ca32d46ea3bd69e39f1fdcce6
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2016-06-28 17:42:49 +0100

    Add new icpEnvExpand tool
    This allow you to process a file in much the same way as
    EPICS MSI but it will use the macros defined in the current
    configuration and globals.txt
    
        icpconfigEnvExpand  in_file.txt out_file.txt
    
    Will replace $() instances in in_file with macros from the
    current configuration

--- icpconfigApp/src/icpconfig.h
+++ icpconfigApp/src/icpconfig.h
@@ -3,5 +3,6 @@
 
 enum icpOptions { VerboseOutput = 0x1 };
 epicsShareExtern int icpconfigCheck(const std::string& configName, const std::string& ioc_name, const std::string& ioc_group, const std::string& configHost, const std::string& configBase, int options);
+epicsShareExtern int icpconfigEnvExpand(const std::string& inFileName, const std::string& outFileName, const std::string& configName);
 
 #endif /* ICPCONFIG_H */
