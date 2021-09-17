f9c1919992f80ab4de68e25ccf9495d6159fa884
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2021-09-17 16:57:48 +0100

    Support NOSIM mode

--- icpconfigApp/src/icpconfig.cpp
+++ icpconfigApp/src/icpconfig.cpp
@@ -338,20 +338,22 @@ static void icpconfigReport()
 
 // If running IOC tests, load in IOC test macros 
 static void loadTestMacros(MAC_HANDLE *h, const std::string& config_name, const std::string& config_root, const std::string& ioc_name, const std::string& ioc_group, bool verbose){
-	std::string testdevsim, testrecsim, var_dir;
-	bool testdevsim_is_empty, testrecsim_is_empty;
+	std::string testdevsim, testrecsim, testnosim, var_dir;
+	bool testdevsim_is_empty, testrecsim_is_empty, testnosim_is_empty;
 
 	testdevsim = macEnvExpand("$(TESTDEVSIM=)");
 	testrecsim = macEnvExpand("$(TESTRECSIM=)");
+	testnosim = macEnvExpand("$(TESTNOSIM=)");
 
 	var_dir = macEnvExpand("$(ICPVARDIR)");
 
 	testdevsim_is_empty = testdevsim.size() == 0;
 	testrecsim_is_empty = testrecsim.size() == 0;
+	testnosim_is_empty = testnosim.size() == 0;
 
-	if ((!testdevsim_is_empty || !testrecsim_is_empty) && (var_dir.size() == 0)) {
+	if ((!testdevsim_is_empty || !testrecsim_is_empty || !testnosim_is_empty) && (var_dir.size() == 0)) {
 		errlogPrintf("icpconfigLoad: failed (ICPVARDIR environment variable not set - cannot load test macros)\n");
-	} else if ((!testdevsim_is_empty || !testrecsim_is_empty)) {
+	} else if ((!testdevsim_is_empty || !testrecsim_is_empty || !testnosim_is_empty)) {
 		loadMacroFile(h, var_dir + "/tmp/test_macros.txt", config_name, config_root, ioc_name, ioc_group, false, false, verbose);
 		simulate = true;
 		setValue(h, "SIMULATE", "1", "{simulation mode for tests}");
