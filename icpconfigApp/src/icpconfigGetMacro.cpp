bb626670d0f9da0244f043991594b156205925fe
Nikola Roev <nikola.roev@stfc.ac.uk>
2022-08-10 09:54:42 +0100

    Removed unnecessary includes, added a clarifying comment for the output.

--- icpconfigApp/src/icpconfigGetMacro.cpp
+++ icpconfigApp/src/icpconfigGetMacro.cpp
@@ -1,54 +1,8 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <errno.h>
-#include <math.h>
-#include <exception>
-#include <algorithm>
-#include <stdexcept>
 #include <iostream>
 #include <map>
-#include <list>
 #include <string>
-#include <time.h>
-#include <sstream>
-#include <fstream>
-
-#include "epicsStdlib.h"
-#include "epicsString.h"
-#include "dbDefs.h"
-#include "epicsMutex.h"
-#include "dbBase.h"
-#include "dbStaticLib.h"
-#include "dbFldTypes.h"
-#include "dbCommon.h"
-#include "dbAccessDefs.h"
-#include <epicsTypes.h>
-#include <epicsTime.h>
-#include <epicsThread.h>
-#include <epicsString.h>
-#include <epicsTimer.h>
-#include <epicsMutex.h>
-#include <iocsh.h>
-#include <initHooks.h>
-#include "envDefs.h"
-#include "macLib.h"
-#include "errlog.h"
-#include "dbAccess.h"
-#include "dbTest.h"
-#include "dbStaticLib.h"
-#include "errlog.h"
-
-#ifdef _WIN32
-#include <io.h>
-#else
-#include <unistd.h>
-#endif
-
-#include "pugixml.hpp"
 
 #include "utilities.h"
-
 #include "icpconfig.h"
 
 #include <epicsExport.h>
@@ -75,6 +29,7 @@ int main(int argc, char* argv[])
 	
     std::map<std::string,std::string> names_and_values;
     icpconfigGetMacros(iocName, configName, configHost, names_and_values);
+	// If the macro does not exist the program will return an empty string.
     std::cout << names_and_values[macroName] << std::endl;
     return 0;
 }
